24c5ab595ef5e078ea12d8852177b6f72b5af878
chunya02.li <chunya02.li@phicomm.com>
2017-06-07 11:32:21 +0800

    add refresh buton

--- app/src/main/java/com/phicomm/demo/devices/DevicesActivity.java
+++ app/src/main/java/com/phicomm/demo/devices/DevicesActivity.java
@@ -5,11 +5,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -23,7 +27,6 @@ import com.phicomm.demo.smartconfig.EsptouchDemoActivity;
 import com.phicomm.demo.user.LoginActivity;
 import com.phicomm.demo.util.ActivityUtils;
 import com.phicomm.iot.library.device.IIotDevice;
-import com.phicomm.iot.library.discover.PhiConstants;
 import com.phicomm.iot.library.discover.internetDiscover.ICommandDeviceSynchronizeInternet;
 import com.phicomm.iot.library.discover.internetDiscover.PhiCommandDeviceSynchronizeInternet;
 
@@ -41,6 +44,7 @@ import butterknife.OnClick;
 public class DevicesActivity extends AppCompatActivity {
     private static String TAG = "DevicesActivity";
     private static final int MENU_OPTION_DEVICE_CONFIG = 1;
+    private static final int REFRESH_BUTTON_SATTUS = 1;
     @BindView(R.id.layout_devices_login1)
     LinearLayout mLayoutLogin1;
     @BindView(R.id.layout_devices_login2)
@@ -51,6 +55,7 @@ public class DevicesActivity extends AppCompatActivity {
     private RadioGroup mDeviceGroup;
     private RadioButton mInternetDevices;
     private RadioButton mLocalDevices;
+    private Button mRefreshButton;
     private ArrayList<IIotDevice> mLocalList = new ArrayList<>();
     private ArrayList<IIotDevice> mInternetList;
     private ICommandDeviceSynchronizeInternet mSynchronizeAction = null;
@@ -61,6 +66,7 @@ public class DevicesActivity extends AppCompatActivity {
             if (intent.getAction().equals(DiscoveryService.ACTION_DISCOVERY_RESULT)) {
                 mLocalList = intent.getParcelableArrayListExtra("result");
                 if (mLocalList != null) {
+                    Log.d(TAG,"mLocalList.size()="+mLocalList.size());
                     mPresenter.handleIotAddress(mLocalList);
                 }
             }
@@ -87,18 +93,42 @@ public class DevicesActivity extends AppCompatActivity {
         mDeviceGroup = (RadioGroup) findViewById(R.id.deviceGroup);
         mInternetDevices = (RadioButton) findViewById(R.id.internetDevice);
         mLocalDevices = (RadioButton) findViewById(R.id.localDevice);
+        mRefreshButton = (Button) findViewById(R.id.refreshList);
     }
 
     private void setListener() {
         mDeviceGroup.setOnCheckedChangeListener(mDevicelistener);
+        mRefreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                refreshDeviceList();
+                mRefreshButton.setEnabled(false);
+                mHandler.sendEmptyMessageDelayed(REFRESH_BUTTON_SATTUS,3000);
+
+            }
+        });
     }
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case REFRESH_BUTTON_SATTUS:
+                    mRefreshButton.setEnabled(true);
+            }
+
+        }
+    };
 
     RadioGroup.OnCheckedChangeListener mDevicelistener = new RadioGroup.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(RadioGroup Group, int Checkid) {
+             //refreshDeviceList();
             if (R.id.internetDevice == Checkid) {
                 if (mSynchronizeAction != null && mSynchronizeAction.getInternetDeviceList() != null) {
                     mPresenter.handleIotAddress((ArrayList<IIotDevice>) mSynchronizeAction.getInternetDeviceList());
+                } else {
+                    startActionInternetDiscovery();
                 }
             } else if (R.id.localDevice == Checkid) {
                 if (DiscoveryService.bServiceRunning) {
@@ -114,13 +144,29 @@ public class DevicesActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         loadLoginPanel();
-        if (R.id.localDevice == mDeviceGroup.getCheckedRadioButtonId()) {
-            DiscoveryService.startActionUdpDiscovery(this);
-        } else if (R.id.internetDevice == mDeviceGroup.getCheckedRadioButtonId()) {
-            startActionInternetDiscovery();
-        }
+        refreshDeviceList();
         IntentFilter filter = new IntentFilter(DiscoveryService.ACTION_DISCOVERY_RESULT);
         LocalBroadcastManager.getInstance(this).registerReceiver(receiver, filter);
+    }
+
+    public void refreshDeviceList() {
+        int mOrigncheckId= mDeviceGroup.getCheckedRadioButtonId();
+        Log.d(TAG,"Token="+getLoginOnToken());
+        if (!getLoginOnToken().isEmpty()) {
+            if (mInternetDevices.getVisibility() == View.GONE) {
+                mInternetDevices.setVisibility(View.VISIBLE);
+                mInternetDevices.setChecked(true);
+                startActionInternetDiscovery();
+            } else if (mOrigncheckId == R.id.localDevice) {
+                DiscoveryService.startActionUdpDiscovery(this);
+            } else if (mOrigncheckId == R.id.internetDevice) {
+                startActionInternetDiscovery();
+            }
+        } else {
+            mInternetDevices.setVisibility(View.GONE);
+            mLocalDevices.setChecked(true);
+            DiscoveryService.startActionUdpDiscovery(this);
+        }
     }
 
     // This is for the internet Discovery
@@ -131,7 +177,7 @@ public class DevicesActivity extends AppCompatActivity {
         taskInternet = new Callable<ArrayList<IIotDevice>>() {
             @Override
             public ArrayList<IIotDevice> call() throws Exception {
-                return doCommandSynchronizeInternet(PhiConstants.UserKey);
+                return doCommandSynchronizeInternet(getLoginOnToken());
             }
         };
         futureInternet = executor.submit(taskInternet);
@@ -190,6 +236,7 @@ public class DevicesActivity extends AppCompatActivity {
     public void clickLogout() {
         ((DemoApp) getApplication()).store().setIotToken("");
         loadLoginPanel();
+        refreshDeviceList();
     }
 
     @OnClick(R.id.bt_login_login)
@@ -198,18 +245,23 @@ public class DevicesActivity extends AppCompatActivity {
     }
 
     private void loadLoginPanel() {
-        if (((DemoApp) getApplication()).store().getIotToken().isEmpty()) {
+        if (getLoginOnToken().isEmpty()) {
             mLayoutLogin1.setVisibility(View.VISIBLE);
             mLayoutLogin2.setVisibility(View.GONE);
         } else {
             mLayoutLogin1.setVisibility(View.GONE);
             mLayoutLogin2.setVisibility(View.VISIBLE);
-            mTextLoginMsg.setText("登陆成功. token: " + ((DemoApp) getApplication()).store().getIotToken());
+            mTextLoginMsg.setText("登陆成功. token: " + getLoginOnToken());
         }
     }
 
     public static void start(Context context) {
         Intent starter = new Intent(context, DevicesActivity.class);
         context.startActivity(starter);
+    }
+
+
+    private String getLoginOnToken(){
+        return ((DemoApp) getApplication()).store().getIotToken();
     }
 }
